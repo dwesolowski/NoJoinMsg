49c7abe42fcbe868ed8810f62e5deaecf9dbce31
Daren Wesolowski <daren.wesolowski@icloud.com>
2017-03-05 09:35:09 -0500

    initial import

---
+++ README.md
@@ -0,0 +1,9 @@
+# NoJoinMsg - Plugin for Bukkit | Spigot
+
+Removes the default server join / leave messages.
+
+#### Supported Versions: 1.11+
+
+## Installation
+
+To use this plugin place the .jar and any other files in your plugins directory and restart the server.
---
+++ src/main/java/com/github/dwesolowski/nojoinmsg/NoJoinMsg.java
@@ -0,0 +1,25 @@
+package com.github.dwesolowski.nojoinmsg;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoJoinMsg extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        e.setJoinMessage("");
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        e.setQuitMessage("");
+    }
+}
